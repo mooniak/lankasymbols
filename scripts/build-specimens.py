5570ec4c7f310906002860770809c2f7fc6bac83
Pathum Egodawatta <pathumego@gmail.com>
2018-02-16 15:51:56 +0530

    Updating scripts

--- scripts/build-specimens.py
+++
@@ -1,3 +0,0 @@
-# for layer in Font.selectedLayers:
-# 	glyph = layer.parent
-# 	print '| <i class="ls %s"></i> 	|  %s |  `u+%s` |  `<i class="ls %s"></i>`| ' % (glyph.name, glyph.name, glyph.unicode,  glyph.name, )
